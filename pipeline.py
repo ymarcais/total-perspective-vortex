ff5f5494025b29df213d0c8cf381f96e1103e3ce
Yves MARCAIS <ymarcais@made-f0Dr10s13.clusters.42paris.fr>
2023-11-07 19:50:24 +0100

    classes for transformers

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import mne
 import matplotlib.pyplot as plt
+import pywt
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
@@ -21,23 +22,92 @@ from sklearn.preprocessing import OneHotEncoder
 from my_ica import My_ica
 from treatment_pipeline import Treatment_pipeline
 from mne.filter import filter_data
+from mne.preprocessing import ICA
 
 
-class Filtering:
-	'''create a class filtering to create a transformer that use 
-	parameters lower_passband, higher_passband'''
-	def __init__(self, lower_passband, higher_passband):
-		self.lower_passband = lower_passband
-		self.higher_passband = higher_passband
-		pp = Preprocessing()
+from sklearn.base import BaseEstimator, TransformerMixin
+import mne
+
+class MNEFilterTransformer(BaseEstimator, TransformerMixin):
+	def __init__(self, sfreq=None, l_freq=7, h_freq=40):
+		self.lower_passband = l_freq
+		self.higher_passband = h_freq
+		self.sfreq = sfreq
 
 	def fit(self, X, y=None):
 		return self
 
 	def transform(self, X):
-		# Your filtering logic here, using self.lower_passband and self.higher_passband
-		filtered_data = self.pp.filetring(X, self.lower_passband, self.higher_passband)
+		if isinstance(X, mne.io.BaseRaw):  # Check if X is an MNE Raw object
+			data = X.get_data()  # Get the raw data from the Raw object
+			sfreq = X.info['sfreq']  # Get the sampling frequency from the Raw object
+		else:
+			data = X
+			sfreq = self.sfreq
+
+		filtered_data = mne.filter.filter_data(data, sfreq=sfreq, l_freq=self.lower_passband, h_freq=self.higher_passband)
+
 		return filtered_data
+
+class Wavelet(BaseEstimator, TransformerMixin):
+	def __init__(self, raw=None) :
+		self.raw = raw
+
+	def fit(self, X, y=None):
+		return self
+	
+	
+	def transform(self, X):
+		if isinstance(X, mne.io.BaseRaw):  # Check if X is an MNE Raw object
+			data = X.get_data()  # Get the raw data from the Raw object
+		else:
+			data = X
+		
+		wavelet = 'db1'
+		level = 2
+		coeffs = pywt.wavedec(data, wavelet)
+		reconstructed_data = pywt.waverec(coeffs[:level + 1], wavelet)
+		wavelet_raw = mne.io.RawArray(reconstructed_data, self.raw.info)
+		wavelet_raw.set_annotations(self.raw.annotations)
+
+		return wavelet_raw
+	
+class Fourrier_Frequency_Transformation(BaseEstimator, TransformerMixin):
+	def __init__(self, raw=None) :
+		self.raw = raw
+
+	def fit(self, X, y=None):
+		return self
+	
+	
+	def transform(self, X):
+		if isinstance(X, mne.io.BaseRaw):  # Check if X is an MNE Raw object
+			data = X.get_data()  # Get the raw data from the Raw object
+		else:
+			data = X
+
+		data_fft = np.fft.fft(data, axis=1)
+		return data_fft
+	
+	
+class Ica_Comp(BaseEstimator, TransformerMixin):
+	def __init__(self, raw=None) :
+		self.raw = raw
+
+
+	def fit(self, X, y=None):
+		return self
+	
+	
+	def transform(self, X):
+		data_fft_abs = np.abs(X)
+		raw_fft_result = mne.io.RawArray(data_fft_abs, self.raw.info)
+		raw_fft_result.set_annotations(self.raw.annotations)
+		random_seed = 42
+		ica = ICA(n_components=0.95, random_state=random_seed)
+		ica_result = ica.fit(raw_fft_result)
+		n_components = ica.n_components_
+		return ica_result
 
 class Pipeline:
 
@@ -62,24 +132,23 @@ class Pipeline:
 				LDA'''
 		pp = Preprocessing()
 		tp = Treatment_pipeline()
-
+		sfreq = raw.info['sfreq']
 		
-		filtered = FunctionTransformer(Filtering(self.lower_passband, self.higher_passband).transform)
-		resemp = self.resampling(filtered)
-		normalized = FunctionTransformer(pp.magnitude_normaliz(filtered))
-		wavelet = FunctionTransformer(pp.wavelet_analysis(filtered))
-		raw_fft_result, data_fft = FunctionTransformer(pp.frequency_fourrier(resemp))
-		_, data_ica = FunctionTransformer(tp.ica_comp(raw_fft_result))
-		lda_result = LDA(data_fft)
+		filter = MNEFilterTransformer(sfreq=sfreq, l_freq =1, h_freq =40)
+		scaler = StandardScaler()
+		wavelet = Wavelet(raw=raw)
+		data_fft = Fourrier_Frequency_Transformation()
+		data_ica = Ica_Comp(raw=raw)
+		#lda_result = LDA(self)
 		
-		numerical_pipeline = make_pipeline(	filtered,
-									 		resemp,
-									 		normalized,
-									 		wavelet,											
-											raw_fft_result, data_fft,
-											data_ica,
-											lda_result
+		
+		numerical_pipeline = make_pipeline(	filter,
+											scaler,
+											wavelet,
+											data_fft,
+											data_ica
 											)
+		
 		return numerical_pipeline
 
 
@@ -89,6 +158,7 @@ class Pipeline:
 				Check inputs
 				OneHotEcnoder gives converts categorical to digital matrix'''
 		
+'''make a class for mapping, this is not a make_pipeline'''
 		mapping = make_pipeline(Preprocessing().rename_existing_mapping(raw))
 		impute = SimpleImputer(strategy='most_frequent')
 		categorical_pipeline = make_pipeline(	mapping,
@@ -140,8 +210,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
-
-
-
 
